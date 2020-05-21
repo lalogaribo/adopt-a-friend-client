a8fdb29bb8b6271d8467abbfaebc5b10e7c9e139
SaberChulainn <Tbandachit@gmail.com>
2020-05-21 15:28:47 -0700

    homepage background and text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import NavbarPage from "./component/navbar.js"
-
+import HomePage from "./component/homepage.js"
 class App extends React.Component {
   render () {
     return (
       <div>
         <NavbarPage/>
         <div>
+          <HomePage/>
         </div>
       </div>
     )
