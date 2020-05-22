98b331a791abd4dd5e2539adf679ca25b32e013b
SaberChulainn <Tbandachit@gmail.com>
2020-05-22 14:04:10 -0700

    Fix some typos

--- src/component/signup.js
+++ src/component/signup.js
@@ -10,11 +10,11 @@ export default class signUp extends Component {
           <form onSubmit={this.handleSubmit}>
             <div className="signup-form">
                 <h1>Sign Up</h1>
-              <input type="firstname" placeholder="FirstName" class="txtb"/>
+              <input type="firstname" placeholder="First Name" class="txtb"/>
               <input type="lastname" placeholder="Last Name" class="txtb"/>
               <input type="username" placeholder="Username" class="txtb"/>
               <input type="password" placeholder="Password" class="txtb"/>
-              <input type="email" placeholder="email" class="txtb"/>
+              <input type="email" placeholder="Email" class="txtb"/>
               <input type="submit" className="signup-btn" value="Create Account"/>
             </div>
           </form>
