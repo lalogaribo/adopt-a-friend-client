96634e65e25723412f8ea2bc0ea5bf2726cb5bf6
SaberChulainn <Tbandachit@gmail.com>
2020-05-26 16:03:16 -0700

    Add post for create shelter and fix url for shelter

--- src/component/createpet.js
+++ src/component/createpet.js
@@ -3,23 +3,35 @@ import "../style/createpet.css"
 
 
 export default class createShelter extends Component {
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        console.log(data);
+        // fetch("http://localhost:3001/api/v1/shelters/new"), {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: {
+
+        //     }
+        // }
+    }
 
     render() {
         return (
             <header className="create-pet">
-            <body>
             <div className="ui form">
               <form onSubmit={this.handleSubmit}>
                 <div className="signup-form">
-                    <h1>Create Your Shelter</h1>
-                  <input type="petName" placeholder="Pet Name" class="txtb"/>
-                  <input type="breed" placeholder="Breed" class="txtb"/>
-                  <input type="animal_type" placeholder="Animal Type" class="txtb"/>
+                    <h1>Add Your Pet</h1>
+                  <input type="petName" placeholder="Pet Name" className="txtb"/>
+                  <input type="breed" placeholder="Breed" className="txtb"/>
+                  <input type="animal_type" placeholder="Animal Type" className="txtb"/>
                   <input type="submit" className="signup-btn" value="Create Account"/>
                 </div>
               </form>
             </div>
-          </body>
           </header>
         )
     }
